7605237e5b21acde9a9e2dcf07a672bd883a90da
atulnitrr <atulkumar458@gmail.com>
2019-07-14 18:11:56 +0530

    Added form visual

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -1,10 +1,38 @@
-import React from "react";
+import React, { Fragment } from "react";
+import { MdSend } from "react-icons/md";
 
 const ExpenseForm = () => {
   return (
-    <div>
-      <h2>Hello from expense from</h2>
-    </div>
+    <Fragment>
+      <form>
+        <div className="form-center">
+          <div className="form-group">
+            <label htmlFor="charge"> charge</label>
+            <input
+              type="text"
+              className="form-control"
+              name="charge"
+              id="charge"
+              placeholder="e.g rent"
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="amount">amount</label>
+            <input
+              type="text"
+              className="form-control"
+              name="amount"
+              id="amount"
+              placeholder="e.g 1000"
+            />
+          </div>
+        </div>
+        <button type="submit" className="btn">
+          Submit
+          <MdSend className="btn-icon" />
+        </button>
+      </form>
+    </Fragment>
   );
 };
 
